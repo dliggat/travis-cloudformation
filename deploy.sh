63443eebd37bc477c9b1d68c0bd6c49808b49c9e
Dave Liggat <dave@liggat.org>
2015-10-31 19:23:48 -0700

    Temporarily commenting out branch

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-aws s3 sync templates/ s3://$CF_S3_BUCKET --delete --region $AWS_REGION
+echo "hello this is a deploy"
+# aws s3 sync templates/ s3://$CF_S3_BUCKET --delete --region $AWS_REGION
