17082e9cf8cd5051e30556bfd740beef2213f117
Dave Liggat <dave@liggat.org>
2015-10-31 18:22:14 -0700

    Introducing error in JSON

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ class TestJsonParse(unittest.TestCase):
         with open('templates/nginx.template') as data_file:
             data = json.load(data_file)
             self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
+            self.assertItemsEqual(data['Outputs'].keys(), ['PublicIp', 'PublicDns'])
 
 if __name__ == '__main__':
     unittest.main()
