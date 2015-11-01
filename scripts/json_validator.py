dbd51912fa3204800315ed9729c14e7b28a02c70
Dave Liggat <dave@liggat.org>
2015-11-01 13:20:50 -0800

    Refactor with failure

--- scripts/json_validator.py
+++ scripts/json_validator.py
@@ -7,20 +7,25 @@ class TestJsonValidator(unittest.TestCase):
 
     TEMPLATES = 'templates/*.template'
 
+    def setUp(self):
+        """Load the template files."""
+        self.templates = glob.glob(self.TEMPLATES)
+
     def test_load(self):
-        templates = glob.glob(self.TEMPLATES)
-        for template in templates:
+        """Test the JSON parse and the existence of particular keys."""
+        for template in self.templates:
             with open(template, 'r') as data_file:
 
                 # Parse the template => fails if invalid JSON.
                 data = json.load(data_file)
 
                 # Look for the 'AWSTemplateFormatVersion' key.
-                self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
+                self.assertEqual(data['AWSTemplateFormatVersion'], '2011-09-09')
 
                 # Examine the CloudFormation 'Outputs' - expect these keys.
                 self.assertItemsEqual(data['Outputs'].keys(),
                                       ['PublicIp', 'PublicDns'])
+
 
 if __name__ == '__main__':
     unittest.main()
