9d22c89e19efcfb0a0a1d7681226a2e5521fb9e7
Dave Liggat <dave@liggat.org>
2015-10-31 18:42:30 -0700

    Add notion of globbing

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
+import glob
 import json
 import unittest
 
 
 class TestJsonParse(unittest.TestCase):
 
+    TEMPLATES = 'templates/*.template'
+
     def test_load(self):
-        with open('nginx.template') as data_file:
-            data = json.load(data_file)
-            self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
+        templates = glob.glob(self.TEMPLATES)
+        for template in templates:
+            with open(template, 'r') as data_file:
+                data = json.load(data_file)
+                self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
+                self.assertItemsEqual(data['Outputs'].keys(), ['PublicIp', 'PublicDns'])
 
 if __name__ == '__main__':
     unittest.main()
