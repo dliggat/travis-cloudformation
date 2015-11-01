1a4cfe67b9ef02cd35c317421a583209306e8c0c
Dave Liggat <dave@liggat.org>
2015-10-31 18:14:36 -0700

    create templates directory

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import unittest
 class TestJsonParse(unittest.TestCase):
 
     def test_load(self):
-        with open('nginx.template') as data_file:
+        with open('templates/nginx.template') as data_file:
             data = json.load(data_file)
             self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
 
