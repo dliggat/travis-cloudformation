6960ff9f2f8c6add22ead067be4b5f90c707edd2
Dave Liggat <dave@liggat.org>
2015-10-31 18:09:01 -0700

    Fixing whitespace

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import json
 import unittest
 
+
 class TestJsonParse(unittest.TestCase):
 
-  def test_load(self):
-      with open('nginx.template') as data_file:
-        data = json.load(data_file)
-        print
-        self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
+    def test_load(self):
+        with open('nginx.template') as data_file:
+            data = json.load(data_file)
+            self.assertEqual(data['AWSTemplateFormatVersion'], '2010-09-09')
 
 if __name__ == '__main__':
     unittest.main()
